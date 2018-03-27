2308c233c4ecd120f3ca43c743704f103725a6cb
joanitagomez@gmail.com <joanitagomez@gmail.com>
2018-03-27 14:07:26 -0700

    Added grid layout

--- src/Home.js
+++ src/Home.js
@@ -7,12 +7,34 @@ export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      title: "",
+      title: "harry",
       recommendations: [],
       id: 0,
       isNoResults:false,
     };
+
+    fetch(
+       "https://api.themoviedb.org/3/search/movie?api_key=c3111a004530dd2c7aede7c5e398885e&query="+this.state.title,
+       {
+         method: "GET"
+       }
+     )
+     .then(response => response.json())
+     .then(res => {
+       if(res.total_results !== 0){
+       // console.log(res);
+       this.setState({
+         isNoResults: false,
+         recommendations: res.results });
+       }else{
+         this.setState({
+           recommendations: [],
+           isNoResults: true
+         });
+       }
+   });
   }
+
 
   handleSubmit(e) {
     if (e.key === "Enter" && this.state.title !== "") {
@@ -41,35 +63,37 @@ export default class Home extends Component {
   }
 
 
-  renderMovies(recoms) {
-    let arr = [];
-    let secureBaseUrl;
-    var posterUrl;
-    secureBaseUrl = this.props.images.secure_base_url;
-
-    for (var i in recoms) {
-      if(recoms[i].poster_path)
-      posterUrl =
-        secureBaseUrl + this.props.images.poster_sizes[0] + recoms[i].poster_path;
-      arr.push(
-        <List value={i} key={"movie" + i}>
-          <Linkstyle>
-            <Link to={"/movie/" + recoms[i].id}>
-              <img src={posterUrl} alt="poster" />
-              <Title>{recoms[i].title}</Title>
-              <ReleaseDate release_date={recoms[i].release_date} />
-            </Link>
-          </Linkstyle>
-        </List>
-      );
-    }
-
-    return arr;
-  }
-
   handleTitleChange = event => {
     this.setState({ title: event.target.value });
   };
+
+
+
+    renderMovies(recoms) {
+      let arr = [];
+      let secureBaseUrl;
+      var posterUrl;
+      secureBaseUrl = this.props.images.secure_base_url;
+
+      for (var i in recoms) {
+        if(recoms[i].poster_path)
+        posterUrl =
+          secureBaseUrl + this.props.images.poster_sizes[1] + recoms[i].poster_path;
+        arr.push(
+          <List value={i} key={"movie" + i}>
+            <Linkstyle>
+              <Link to={"/movie/" + recoms[i].id}>
+                <img src={posterUrl} alt="poster" />
+                <Title>{recoms[i].title}</Title>
+                <ReleaseDate release_date={recoms[i].release_date} />
+              </Link>
+            </Linkstyle>
+          </List>
+        );
+      }
+
+      return arr;
+    }
 
 
   render() {
@@ -78,6 +102,7 @@ export default class Home extends Component {
       <Wrapper>
         <div>
           <InputWrapper>
+            <Input>
             What movie are you after?
             <input
               type="text"
@@ -85,10 +110,11 @@ export default class Home extends Component {
               onKeyPress={this.handleSubmit.bind(this)}
               onChange={this.handleTitleChange}
             />
+          </Input>
           </InputWrapper>
           <Recommendations>
             {this.props.images && (
-              <ul>{this.renderMovies(this.state.recommendations)}</ul>
+              <Grid>{this.renderMovies(this.state.recommendations)}</Grid>
             )}
             {this.state.isNoResults && (<div>No results</div>)}
           </Recommendations>
@@ -103,32 +129,53 @@ const Wrapper = styled.div`
   height: 100%;
   background-color: #000;
   color: rgba(255, 255, 255, 0.6);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+
+  //border: 1px solid red;
 
   > h1 {
     margin: 0;
   }
 `;
-const Recommendations = styled.div`color: white;`;
-const List = styled.li`
-  list-style: none;
-  background-color: white;
-  border: 1px solid grey;
+
+const Recommendations = styled.div`
+color:white;
+
+`;
+const Grid = styled.div`
+  display:grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  grid-gap: 30px;
+  max-width: auto;
+  margin: 0 100px;
+//  border: 1px solid white;
+
+`;
+
+const List = styled.div`
+  width: auto;
+
+  background-color: transparent;
+  position: relative;
+  cursor: pointer;
+  min-height: 100%;
 `;
 const Title = styled.div`
-  display: inline-block;
-  font-size: 2em;
+  font-size: 1em;
 `;
 const Linkstyle = styled.span`
   > a {
-    color: black;
+    color: white;
+    text-decoration: none;
   }
 `;
 
 const InputWrapper = styled.div`
-  font-size: 2em;
+ display: flex;
+justify-content: center;
+align-items: center;
+`;
+const Input = styled.div`
+  font-size: 1.2em;
   font-weight: 200;
   padding: 2em 0;
 
@@ -150,10 +197,10 @@ const InputWrapper = styled.div`
     border-bottom: 1px solid white;
   }
 
-  @media screen and (max-width: 45em) {
-    font-size: 3em;
+  @media screen and (min-width: 45em) {
+    font-size: 2em;
     > input {
-      font-size: 2em;
+      font-size: 1em;
       font-weight: 200;
     }
   }
